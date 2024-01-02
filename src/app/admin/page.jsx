7dc48688827d603e3fe11515e6ef5bd239444951
SalvaWebPr0 <salvawebpro@gmail.com>
2024-01-02 13:35:06 -0600

    updated profile page

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
 import Profile from '@/components/user/profile/Profile';
-import { getServerSession } from 'next-auth/next';
-import { options } from '@/app/api/auth/[...nextauth]/options';
-
-const getProducts = async () => {
-  const session = await getServerSession(options);
-  const URL = `${process.env.NEXTAUTH_URL}/api/products`;
-  const res = await fetch(URL, {
-    headers: {
-      'X-Mysession-Key': JSON.stringify(session),
-    },
-  });
-  const data = await res.json();
-  return data.products;
-};
 
 const ProfilePage = async () => {
-  const products = await getProducts();
   return <Profile />;
 };
 
