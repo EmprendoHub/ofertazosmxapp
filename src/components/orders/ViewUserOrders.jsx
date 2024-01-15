15a1bd0517e56056af7afb8d7c8d3c35fe8984f6
SalvaWebPr0 <salvawebpro@gmail.com>
2024-01-15 07:56:40 -0600

    mo responsive changes

--- src/components/orders/ViewUserOrders.jsx
+++ src/components/orders/ViewUserOrders.jsx
@@ -32,54 +32,54 @@ const ViewUserOrders = ({ searchParams, currentCookies, params }) => {
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <h1 className="text-3xl my-5 ml-4 font-bold">
-        {`${orders?.length}
+        {`${filteredOrdersCount}
         Pedidos para ${orders[0]?.user.name}`}
       </h1>
 
       <table className="w-full text-sm text-left">
         <thead className="text-l text-gray-700 uppercase">
           <tr>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-6 maxsm:px-1 py-3">
               No.
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-6 maxsm:px-1 maxsm:hidden py-3">
               Total
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-6 maxsm:px-1 py-3">
               Pagado
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-6 maxsm:px-1 py-3">
               Estado
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-6 maxsm:px-1  maxsm:hidden py-3">
               Fecha
             </th>
-            <th scope="col" className="px-6 py-3">
-              Acciones
+            <th scope="col" className="px-2 maxsm:px-1 py-3 text-center">
+              ...
             </th>
           </tr>
         </thead>
         <tbody>
           {orders?.map((order, index) => (
             <tr className="bg-white" key={index}>
-              <td className="px-6 py-2">
+              <td className="px-6 maxsm:px-1 py-2">
                 <Link
                   href={`/admin/pedidos/${order._id}`}
-                  className="px-2 py-2 inline-block text-white hover:text-black bg-black shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 cursor-pointer mr-2"
+                  className="px-2 py-2 inline-block text-black shadow-sm border border-gray-200 rounded-md bg-gray-100 cursor-pointer mr-2"
                 >
                   {order.orderId}
                 </Link>
               </td>
-              <td className="px-6 py-2">
+              <td className="px-6 maxsm:px-1  maxsm:hidden py-2">
                 <FormattedPrice amount={getTotalFromItems(order.orderItems)} />
               </td>
-              <td className="px-6 py-2 ">
+              <td className="px-6 maxsm:px-1 py-2 ">
                 <b>
                   <FormattedPrice amount={order.paymentInfo.amountPaid} />
                 </b>
               </td>
               <td
-                className={`px-6 py-2 font-bold ${
+                className={`px-6 maxsm:px-1 py-2 font-bold ${
                   order.orderStatus === 'Apartado'
                     ? 'text-amber-700'
                     : order.orderStatus === 'En Camino'
@@ -91,11 +91,11 @@ const ViewUserOrders = ({ searchParams, currentCookies, params }) => {
               >
                 {order.orderStatus}
               </td>
-              <td className="px-6 py-2">
+              <td className="px-6 maxsm:px-1  maxsm:hidden py-2">
                 {order?.createdAt &&
                   `${formatDate(order?.createdAt.substring(0, 24))} `}
               </td>
-              <td className="px-6 py-2">
+              <td className="px-2 maxsm:px-1 py-2">
                 <div>
                   <Link
                     href={`/admin/pedidos/${order._id}`}
@@ -103,9 +103,6 @@ const ViewUserOrders = ({ searchParams, currentCookies, params }) => {
                   >
                     <FaPencilAlt className="" />
                   </Link>
-                  <a className="px-2 py-2 inline-block text-white  hover:text-black bg-black shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 cursor-pointer">
-                    <AiOutlineInteraction className="" />
-                  </a>
                 </div>
               </td>
             </tr>
