3f0d9f82a77d8a6768c0fe6b24c6d3f223b98059
SalvaWebPr0 <salvawebpro@gmail.com>
2024-01-15 08:04:13 -0600

    undo changes

--- src/components/admin/profile/Orders.jsx
+++ src/components/admin/profile/Orders.jsx
@@ -25,7 +25,7 @@ const Orders = ({ searchParams, currentCookies }) => {
       setFilteredOrdersCount(ordersData?.filteredOrdersCount);
     }
     getOrders();
-  }, [getAllOrders]);
+  }, [getAllOrders, searchParams, currentCookies]);
 
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
