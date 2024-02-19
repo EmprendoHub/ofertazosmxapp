eb6d6e045c3b6850c1634a13d0e82be473957d92
SalvaWebPr0 <salvawebpro@gmail.com>
2024-02-19 11:23:33 -0600

    update store responsiveness

--- src/components/store/StoreMainHero.jsx
+++ src/components/store/StoreMainHero.jsx
@@ -4,34 +4,36 @@ import React from 'react';
 
 const StoreMainHero = () => {
   return (
-    <div className="container flex flex-col items-center justify-center min-h-[400px]">
-      <div className=" flex flex-row items-center w-full bg-slate-300 h-[400px]">
-        <div className="flex flex-row w-3/4 bg-teal-100 h-full">
-          <div className="p-10 flex flex-col items-start justify-end h-[400px] w-6/12 oferta">
-            <p className=" text-3xl font-EB_Garamond">La Oferta del Mes</p>
-            <h2 className=" text-6xl font-black font-EB_Garamond mb-20">
+    <section className="container flex flex-col items-center justify-center min-h-[400px] maxsm:h-full">
+      <div className="relative flex flex-row maxsm:flex-col items-center w-full bg-slate-300 h-[400px] maxsm:h-full">
+        <div className="relative flex flex-row maxsm:flex-col maxsm:w-full  w-3/4 bg-teal-100 h-full">
+          <div className="p-10 flex flex-col items-start justify-end h-[400px] maxsm:h-full maxsm:w-full w-6/12 oferta">
+            <p className=" text-3xl maxsm:text-lg uppercase">
+              La Oferta del Mes
+            </p>
+            <h2 className=" text-6xl maxsm:text-4xl font-black font-EB_Garamond mb-20">
               MAS OFERTAS ADENTRO...
             </h2>
-            <p className="text-xl tracking-widest font-bold font-raleway">
+            <p className="text-xl maxsm:text-base tracking-widest font-bold font-raleway">
               Lista de deseos {'>>'}
             </p>
           </div>
-          <div className="p-8 relative flex items-center justify-center h-[400px] w-6/12">
+          <div className="p-8 maxsm:p-4 maxsm:pb-0 relative flex items-center justify-center h-[400px] w-6/12 maxsm:w-full maxsm:h-full">
             <Image
               src={`/images/main_stylish_model.png`}
               alt="img"
               width={300}
               height={300}
-              className="object-cover"
+              className="object-cover "
             />
           </div>
         </div>
-        <div className="w-1/4  p-8 flex flex-col items-start justify-start  bg-slate-100 h-full">
+        <div className="w-1/4 maxsm:w-full  p-8 flex flex-col items-start justify-start  bg-slate-100 min-h-[300px] maxsm:h-full">
           <h3 className="text-bold text-2xl">Sugerencias de Michelle</h3>
         </div>
       </div>
-      <div className="container flex flex-row items-center w-full bg-slate-300 h-[400px]">
-        <div className="w-1/4 p-8 relative flex flex-col items-start justify-start bg-red-200 h-full">
+      <div className="container flex flex-row maxsm:flex-col items-center w-full bg-slate-300 h-[400px] maxsm:h-full">
+        <div className="w-1/4 maxsm:w-full p-4 maxsm:pb-0 relative flex flex-col items-start justify-start bg-red-200 h-full">
           <h3 className="text-bold text-2xl">Hasta un 30%</h3>
           <p>En todas las bolsas de mano.</p>
           <Image
@@ -39,10 +41,10 @@ const StoreMainHero = () => {
             width={250}
             height={250}
             src={`/images/main_stylish_model.png`}
-            className="absolute object-cover bottom-0"
+            className="absolute maxsm:relative object-cover bottom-0"
           />
         </div>
-        <div className="w-1/4  p-8 relative flex flex-col items-start justify-start  bg-slate-200 h-full">
+        <div className="w-1/4 maxsm:w-full p-8 maxsm:p-4 maxsm:pb-0 relative flex flex-col items-start justify-start  bg-slate-200 h-full">
           <h3 className="text-bold text-2xl">Explora las marcas </h3>
           <p>Gucci, Fendi, Versace, etc...</p>
           <Image
@@ -50,10 +52,10 @@ const StoreMainHero = () => {
             width={250}
             height={250}
             src={`/images/main_stylish_model.png`}
-            className="absolute object-cover bottom-0"
+            className="absolute maxsm:relative object-cover bottom-0"
           />
         </div>
-        <div className="w-1/4  p-8 relative flex flex-col items-start justify-end  bg-green-200 h-full">
+        <div className="w-1/4 maxsm:w-full p-8 maxsm:p-4 maxsm:pb-0 relative flex flex-col items-start justify-end  bg-green-200 h-full">
           <h3 className="text-bold text-2xl">No te pierdas</h3>
           <p>Las ofertas de temporada.</p>
           <Image
@@ -61,15 +63,15 @@ const StoreMainHero = () => {
             width={220}
             height={220}
             src={`/images/main_stylish_model.png`}
-            className="absolute object-cover top-0"
+            className="absolute maxsm:relative object-cover top-0"
           />
         </div>
-        <div className="w-1/4  p-8 flex flex-col items-start justify-start  bg-slate-100 h-full">
+        <div className="w-1/4 maxsm:w-full p-8 maxsm:p-4 maxsm:pb-0 flex flex-col items-start justify-start  bg-slate-100 h-full">
           <h3 className="text-bold text-2xl">Hasta un 30%</h3>
           <p>En todas las bolsas de mano.</p>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
