e91bd7501180164996c548d29a90101daf5a2f57
Salvador <54673042+EmprendoHub@users.noreply.github.com>
2024-03-13 06:12:07 -0600

    update api minio call

--- src/components/admin/NewVariationProduct.jsx
+++ src/components/admin/NewVariationProduct.jsx
@@ -144,7 +144,7 @@ const NewVariationProduct = () => {
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
-      method: 'PUT',
+      method: 'POST',
       body: file,
     })
       .then(() => {
