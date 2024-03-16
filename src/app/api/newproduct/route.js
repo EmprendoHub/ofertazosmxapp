ce1df20d3f6953c1709ab379e48b54615c044f22
Salvador <54673042+EmprendoHub@users.noreply.github.com>
2024-03-16 11:32:39 -0600

    more route cleaning

--- src/app/api/newproduct/route.js
+++ src/app/api/newproduct/route.js
@@ -2,9 +2,6 @@ import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/db';
 import Product from '@/backend/models/Product';
 import { getToken } from 'next-auth/jwt';
-import { join } from 'path';
-import { writeFile } from 'fs/promises';
-import { mc } from '@/lib/minio';
 import { generateUrlSafeTitle } from '@/backend/helpers';
 
 export async function POST(request, res) {
@@ -258,27 +255,6 @@ export async function PUT(request, res) {
         },
         { status: 500 }
       );
-    }
-  } else {
-    // Not Signed in
-    return new Response('You are not authorized, eh eh eh, no no no', {
-      status: 400,
-    });
-  }
-}
-
-export async function DELETE(req) {
-  const sessionData = req.headers.get('x-mysession-key');
-  const session = JSON.parse(sessionData);
-  if (session) {
-    try {
-      await dbConnect();
-      const urlData = await req.url.split('?');
-      const id = urlData[1];
-      const deleteProduct = await Product.findByIdAndDelete(id);
-      return new Response(JSON.stringify(deleteProduct), { status: 201 });
-    } catch (error) {
-      return new Response(JSON.stringify(error.message), { status: 500 });
     }
   } else {
     // Not Signed in
