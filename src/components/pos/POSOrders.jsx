0b0a7cc4f13333ab01e9263c767b51f04a1c5f7d
Emprendo Mac <emprendomex@gmail.com>
2024-04-15 15:18:39 -0600

    udate

--- src/components/pos/POSOrders.jsx
+++ src/components/pos/POSOrders.jsx
@@ -8,6 +8,7 @@ import AdminOrderSearch from "@/components/layout/AdminOrderSearch";
 import { TfiMoney } from "react-icons/tfi";
 import { useState } from "react";
 import Modal from "../modals/Modal";
+import { FaEye } from "react-icons/fa6";
 
 const POSOrders = ({ orders, filteredOrdersCount }) => {
   const [showModal, setShowModal] = useState(false);
@@ -105,6 +106,12 @@ const POSOrders = ({ orders, filteredOrdersCount }) => {
                 </td>
                 <td className="px-1 py-2">
                   <div className="flex items-center ">
+                    <Link
+                      href={`/puntodeventa/pedido/${order._id}`}
+                      className="px-2 py-2 inline-block text-white hover:text-black bg-black shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 cursor-pointer mr-2"
+                    >
+                      <FaEye className="" />
+                    </Link>
                     <Link
                       href={`/puntodeventa/recibo/${order._id}`}
                       className="px-2 py-2 inline-block text-white hover:text-black bg-black shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 cursor-pointer mr-2"
